12ab864bd038cb4f646b50899d410ded6bf08427
Akshay Bhuva <akshay.bhuva15@gmail.com>
2022-08-17 20:23:18 +1200

    add Form component

--- src/componensts/Meals/AvailableMeals.js
+++ src/componensts/Meals/AvailableMeals.js
@@ -30,7 +30,9 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-   const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} name={meal.name} price={meal.price} description={meal.description} />);
+   const mealsList = DUMMY_MEALS.map((meal) => (
+      <MealItem id={meal.id} key={meal.id} name={meal.name} price={meal.price} description={meal.description} />
+   ));
 
    return (
       <section className={classes.meals}>
