8485456cd12a36cd8d0c433b7beb75eaff915c3f
Akshay Bhuva <akshay.bhuva15@gmail.com>
2022-08-17 17:52:01 +1200

    add meals components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from 'react';
 import Header from './componensts/Layout/Header';
+import Meals from './componensts/Meals/Meals';
 
 function App() {
    return (
       <Fragment>
          <Header />
+         <main>
+            <Meals />
+         </main>
       </Fragment>
    );
 }
