d1265d35db56a7fe3f06d423816e6b9492c28f16
Akshay Bhuva <akshay.bhuva15@gmail.com>
2022-08-18 14:49:08 +1200

    add Modal component to shopping cart and manage its state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+import Cart from './componensts/Cart/Cart';
 import Header from './componensts/Layout/Header';
 import Meals from './componensts/Meals/Meals';
 
 function App() {
+   const [cartIsShown, setCartIsShown] = useState(false);
+
+   const showCartHandler = () => {
+      setCartIsShown(true);
+   };
+
+   const hideCartHandler = () => {
+      setCartIsShown(false);
+   };
+
    return (
       <Fragment>
-         <Header />
+         {cartIsShown && <Cart onClose={hideCartHandler} />}
+         <Header onShowCart={showCartHandler} />
          <main>
             <Meals />
          </main>
